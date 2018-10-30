c912dbc38194aabc795988c28475ae944587d943
jherreri <jorge.herreria-arista@oracle.com>
2018-10-30 16:55:08 -0700

    Ejemplo Completo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+import  store  from './store';
+
+import Posts from './components/posts';
+import PostForm from './components/postform';
+
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+
+                        <a className="App-link"
+                            href="https://reactjs.org"
+                            target="_blank"
+                            rel="noopener noreferrer">
+                            Learn React
+                        </a>
+                    </header>
+                    <PostForm/>
+                    <hr/>
+                    <Posts/>
+                </div>
+            </Provider>
+        );
+    }
 }
 
 export default App;
